daed42f25d601a7336613a89922037e5c6e4ecfa
Henrique Lima <henriquefreitasl28@gmail.com>
2025-08-24 23:54:41 -0300

    add swagger and scalar-ui

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -1,4 +1,6 @@
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { apiReference } from '@scalar/nestjs-api-reference'
 import { AppModule } from './app.module'
 import { EnvService } from './env/env.service'
 
@@ -6,6 +8,26 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     // logger: false
   })
+
+  const config = new DocumentBuilder()
+    .setTitle('Practice API')
+    .setDescription('An API to practice concepts')
+    .setVersion('1.0.0')
+    .addGlobalResponse({
+      status: 500,
+      description: 'Internal server error',
+    })
+    .build()
+
+  const documentFactory = SwaggerModule.createDocument(app, config)
+  const OpenApiSpecification = app.use(
+    apiReference({
+      content: documentFactory,
+      theme: 'kepler',
+    })
+  )
+
+  SwaggerModule.setup('/api', OpenApiSpecification, documentFactory)
 
   const envService = app.get(EnvService)
   const port = envService.get('PORT')
